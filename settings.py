73ecf2988e1bd213f6fdad29c5b27298be8d444c
Andrew <andreyshakh04@mail.ru>
2024-03-24 19:27:02 +0300

    First commit

--- settings.py
+++ settings.py
@@ -13,8 +13,8 @@ class Settings():
         self.ship_limit = 3
 
         # Настройки снаряда
-        self.bullet_speed = 10
-        self.bullet_width = 300
+        self.bullet_speed = 1
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
